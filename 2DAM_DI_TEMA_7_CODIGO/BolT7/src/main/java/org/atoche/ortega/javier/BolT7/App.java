3d9406be336a1171ba5da3d1d7153434cbb60ace
javiDocenteInformatica <javi.docente.informatica@gmail.com>
2023-11-26 13:14:34 +0100

    inicial BolT7 con MAVEN
    26/11/2023 13:14

---
+++ 2DAM_DI_TEMA_7_CODIGO/BolT7/src/main/java/org/atoche/ortega/javier/BolT7/App.java
@@ -0,0 +1,24 @@
+package org.atoche.ortega.javier.BolT7;
+
+import javax.swing.SwingUtilities;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				
+				
+			}
+        	
+        });
+    }
+}
