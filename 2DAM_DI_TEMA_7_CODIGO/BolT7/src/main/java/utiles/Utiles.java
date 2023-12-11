1249c57c2132ee0471e46e66730175f8ee3bbab1
javiDocenteInformatica <javi.docente.informatica@gmail.com>
2023-12-11 22:00:24 +0100

    actualización 11/12/2023 21:56h
    + completado: public void testClickMenuItem()
    
    + Añadidas variables para colores en consola en la clase "Utiles"
    
    + Actualizado el BookMarks

--- 2DAM_DI_TEMA_7_CODIGO/BolT7/src/main/java/utiles/Utiles.java
+++ 2DAM_DI_TEMA_7_CODIGO/BolT7/src/main/java/utiles/Utiles.java
@@ -49,6 +49,10 @@ public class Utiles extends JComponent {
 	public static final String COLOR_MARRON1 = "574240";
 	public static final String COLOR_MARRON2 = "BFA5A4";
 	public static final String COLOR_AZUL1 = "005983";
+	
+	// COLORES CONSOLA
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_GREEN = "\u001B[32m";
 
 	/**
 	 * Obtiene el nombre de la clase de un objeto.
--- 2DAM_DI_TEMA_7_CODIGO/BolT7/src/test/java/vista/PopupMenuDemoTest.java
+++ 2DAM_DI_TEMA_7_CODIGO/BolT7/src/test/java/vista/PopupMenuDemoTest.java
@@ -1,12 +1,22 @@
 package vista;
 
+import utiles.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+
 import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.ArrayList;
 
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.MenuElement;
 import javax.swing.MenuSelectionManager;
 
@@ -55,9 +65,9 @@ public class PopupMenuDemoTest {
 	}
 
 	/**
-	 * En este test se pretende probar que se selecciona correctamente un menú (JMenu). 
-	 * Esta  selección es un sombreado del menú (componente). Es diferente seleccionar que
-	 * hacer click.
+	 * En este test se pretende probar que se selecciona correctamente un menú
+	 * (JMenu). Esta selección es un sombreado del menú (componente). Es diferente
+	 * seleccionar que hacer click.
 	 */
 	@Test
 	public void testSelectMenu() {
@@ -78,29 +88,88 @@ public class PopupMenuDemoTest {
 	}
 
 	/**
-	 * En este test se pretende probar que se hace click correctamente en un menú (JMenu). 
-	 * Al hacer click en el menú, este debe abrirse.
-	 * Se debe comprobar que efectivamente se abre el menú..
+	 * En este test se pretende probar que se hace click correctamente en un menú
+	 * (JMenu). Al hacer click en el menú, este debe abrirse. Se debe comprobar que
+	 * efectivamente se abre el menú..
 	 */
 	@Test
 	public void testClickMenuItem() {
 		// TODO
-		
-		
-		
-		
+
+		/*
+		 * -> Creamos una variable para almacenar si se ha activado el evento.
+		 * 
+		 * -> Se crea un array debido a que no es posible acceder una variable en el
+		 * contexto del "actionPeformed", pero con un array si es posible ya que un
+		 * array se pasa como referencia a memoria, por tanto nos permite modificar su
+		 * valor, aunque sea "final".
+		 * 
+		 * -> "final" lo utilizamos para optimizar la memoria que se utiliza, ya que a
+		 * pesar de ser "final" al ser un valor referenciado en memoria, podemos
+		 * modificarlo.
+		 */
+		final boolean itemSelected[] = { false };
+
+		// Creamos un JMenuBar utilizando el método createMenuBar de popupMenuDemo.
+		JMenuBar jMenuBar = popupMenuDemo.createMenuBar();
+
+		// Obtenemos el primer menú de "jMenuBar", y si es nulo, creamos un nuevo JMenu
+		JMenu jMenu1 = jMenuBar.getMenu(0);
+		jMenu1 = (jMenu1 != null) ? jMenu1 : new JMenu("No hay menú");
+
+		// Obtenemos el primer JMenuItem de "jMenu1", y si es nulo, creamos un nuevo
+		// JMenuItem
+		JMenuItem jMenuItem1 = jMenu1.getItem(0);
+		jMenuItem1 = (jMenuItem1 != null) ? jMenuItem1 : new JMenuItem("No hay item");
+
+		// Agregamos un ItemListener al JMenuItem para "escuchar" el cambio de estado
+		jMenuItem1.addItemListener(new ItemListener() {
+			@Override
+			public void itemStateChanged(ItemEvent evt) {
+
+				// Obtenemos el JMenuItem que ha lanzado el evento al ser seleccionado
+				JMenuItem jMenuItem = (JMenuItem) evt.getSource();
+				// Mostramos el texto del "jMenuItem" con colores por consola
+				System.out.println(Utiles.ANSI_GREEN + "Cuando se lanza 'itemStateChanged' el JMenuItem lanzado tiene el texto: "
+						+ Utiles.ANSI_RED + jMenuItem.getText());
+				System.out.println();
+
+				// Verificamos si el estado del item es "SELECTED"
+				if (evt.getStateChange() == ItemEvent.SELECTED) {
+					// Cuando se activa el evento, establecemos la variable en "true"
+					itemSelected[0] = true;
+				}
+			}
+		});
+
+		// Mostramos la variable "itemSelected[0]" ANTES de que sea seleccionada
+		String mensajeConsola = "ANTES de ejecutar automáticamente la selección de un JMenuItem";
+		System.out.println(String.format("%s: %b", mensajeConsola, itemSelected[0]));
+
+		// Simulamos el evento ItemEvent con estado "SELECTED"
+		ItemEvent itemEvent = new ItemEvent(jMenuItem1, ItemEvent.ITEM_STATE_CHANGED, jMenuItem1, ItemEvent.SELECTED);
+		ItemListener itemListener = jMenuItem1.getItemListeners()[0];
+		itemListener.itemStateChanged(itemEvent);
+
+		// Mostramos la variable "itemSelected[0]" DESPUÉS de que sea seleccionada
+		mensajeConsola = "DESPUÉS de ejecutar automáticamente la selección de un JMenuItem";
+		System.out.println(String.format("%s: %b", mensajeConsola, itemSelected[0]));
+
+		// Verificamos que la variable de "itemSelected" ahora sea true
+		assertEquals(true, itemSelected[0]);
+
 	}
 
-	
 	/**
-	 * Este test pretende probar, cuando el cursor está por encima de un submenú (un JMenu dentro de otro JMenu), el submenú se abre correctamente.
-	 * Se debe simular que colocas el cursor encima del componente.
-	 * Se debe comprobar que el menú está desplegado.
+	 * Este test pretende probar, cuando el cursor está por encima de un submenú (un
+	 * JMenu dentro de otro JMenu), el submenú se abre correctamente. Se debe
+	 * simular que colocas el cursor encima del componente. Se debe comprobar que el
+	 * menú está desplegado.
 	 */
 	@Test
 	public void testHoverSubmenu() {
 		// TODO
-		
+
 	}
 
 }
